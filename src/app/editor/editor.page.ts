714da8c803c203e21def5b8a194d1572e0b1437d
IAM5K <sandeep2k2656@gmail.com>
2022-08-29 12:30:49 +0530

    v 2.1.4

---
+++ src/app/editor/editor.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.page.html',
+  styleUrls: ['./editor.page.scss'],
+})
+export class EditorPage implements OnInit {
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+}
